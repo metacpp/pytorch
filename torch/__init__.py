9416153f02cd0ba23e43e55b4654bc19228d707a
Christian Puhrsch <cpuhrsch@fb.com>
2022-02-15 17:19:33 +0000

    Minimal NestedTensor object

--- torch/__init__.py
+++ torch/__init__.py
@@ -818,6 +818,25 @@ import torch.utils.data
 from torch import __config__ as __config__
 from torch import __future__ as __future__
 from torch import profiler as profiler
+# from torch import nestedtensor
+
+from torch.nestedtensor.nested.nested import NestedTensor
+# from .nestedtensor.nested.creation import as_nested_tensor
+# from .nestedtensor.nested.creation import nested_tensor
+
+# from .nested.masking import nested_tensor_from_tensor_mask
+# from .nested.masking import nested_tensor_from_padded_tensor
+# 
+# from .nested.nested import NestedTensor
+# from .nested.nested import to_nested_tensor
+# from .nested.nested import transpose_nchw_nhwc
+# from .nested.nested import transpose_nhwc_nchw
+# 
+# from .nested.fuser import fuse_conv_bn
+# from .nested.fuser import fuse_conv_relu
+# from .nested.fuser import fuse_conv_add_relu
+# 
+# from torch.nestedtensor import nestedtensor
 
 _C._init_names(list(torch._storage_classes))
 
